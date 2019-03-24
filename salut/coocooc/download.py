696f54aed4e966613f6589a519da0806903ff696
clairefischer <48767816+clairefischer@users.noreply.github.com>
2019-03-24 19:08:39 +0100

    Delete download.py

--- salut/coocooc/download.py
+++
@@ -1,4 +0,0 @@
-
-import urllib
-url1=urllib.request.urlopen("https://ufal.mff.cuni.cz/~zabokrtsky/courses/npfl104/data/classification/artificial_objects.tgz")
-url2=urllib.request.urlopen("http://archive.ics.uci.edu/ml/datasets/Adult")
